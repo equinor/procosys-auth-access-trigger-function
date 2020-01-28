535f13c99e1960ef0e32c1b7717e8eb7cf9677f6
Tord Joranger <TORDJ@equinor.com>
2020-01-28 11:38:24 +0100

    refactor

--- src/AccessFunctions/ProcosysGroupSubscriber.cs
+++ src/AccessFunctions/ProcosysGroupSubscriber.cs
@@ -16,12 +16,16 @@ namespace AccessFunctions
         private const string Resource = "groups";
         private const string ChangeType = "updated";
 
+        private static ILogger _logger;
+
         [Disable]
         [FunctionName("ProcosysGroupSubscriber")]
         [SuppressMessage("Redundancy", "RCS1163:Unused parameter.")]
         public static async Task Run([TimerTrigger(Cron)]TimerInfo myTimer,
-           ILogger log)
+           ILogger logger)
         {
+            _logger = logger;
+
             double subScriptionTime = double.Parse(GetEnvironmentVariable("SubscriptionTimeToLive"));
             var authority = GetEnvironmentVariable("AzureAuthority");
             var graphUrl = GetEnvironmentVariable("GraphUrl");
@@ -31,6 +35,24 @@ namespace AccessFunctions
             var notificationUrl = GetEnvironmentVariable("NotificationUrl");
             var authContext = new AuthenticationContext(authority);
 
+            var graphClient = await SetUpGraphClient(graphUrl, clientId, clientSecret, authContext);
+
+            var currentSubscriptions = await graphClient.Subscriptions
+                .Request()
+                .GetAsync();
+
+            if (GetSubscriptionToUpdate(notificationUrl, currentSubscriptions) is string updateId)
+            {
+                await UpdateSubscription(subScriptionTime, graphClient, updateId);
+            }
+            else
+            {
+                await CreateSubscription(subScriptionTime, clientState, notificationUrl, graphClient);
+            }
+        }
+
+        private static async Task<IGraphServiceClient> SetUpGraphClient(string graphUrl, string clientId, string clientSecret, AuthenticationContext authContext)
+        {
             ClientCredential clientCred = new ClientCredential(clientId, clientSecret);
             var authenticationResult = await authContext.AcquireTokenAsync(graphUrl, clientCred);
             var accessToken = authenticationResult.AccessToken;
@@ -42,38 +64,38 @@ namespace AccessFunctions
                         requestMessage.Headers.Authorization = new AuthenticationHeaderValue("bearer", accessToken);
                         return Task.FromResult(0);
                     }));
-
-            var subscriptions = await graphClient.Subscriptions
-                .Request()
-                .GetAsync();
-
-            if (subscriptions.Count > 0 && notificationUrl.Equals(subscriptions[0].NotificationUrl))
-            {
-                var subscription = new Subscription
-                {
-                    ExpirationDateTime = DateTimeOffset.UtcNow.AddMinutes(subScriptionTime)
-                };
-                var updateId = subscriptions[0].Id;
-                await graphClient.Subscriptions[updateId]
-                    .Request()
-                    .UpdateAsync(subscription);
-            }
-            else
-            {
-                var subscription = new Subscription
-                {
-                    ChangeType = ChangeType,
-                    NotificationUrl = notificationUrl,
-                    Resource = Resource,
-                    ExpirationDateTime = DateTimeOffset.UtcNow.AddMinutes(subScriptionTime),
-                    ClientState = clientState
-                };
-
-                var request = graphClient.Subscriptions.Request();
-
-                log.LogInformation($"Subscribing to microsoft graph with with request: {request}");
-                await request.AddAsync(subscription);
-            }
+            return graphClient;
         }
+
+        private static async Task CreateSubscription(double subScriptionTime, string clientState, string notificationUrl, IGraphServiceClient graphClient)
+        {
+            var subscription = new Subscription
+            {
+                ChangeType = ChangeType,
+                NotificationUrl = notificationUrl,
+                Resource = Resource,
+                ExpirationDateTime = DateTimeOffset.UtcNow.AddMinutes(subScriptionTime),
+                ClientState = clientState
+            };
+
+            var request = graphClient.Subscriptions.Request();
+
+            _logger.LogInformation($"Subscribing to microsoft graph with with request: {request}");
+            await request.AddAsync(subscription);
+        }
+
+        private static async Task UpdateSubscription(double subScriptionTime, IGraphServiceClient graphClient, string updateId)
+        {
+            var subscription = new Subscription
+            {
+                ExpirationDateTime = DateTimeOffset.UtcNow.AddMinutes(subScriptionTime)
+            };
+            await graphClient.Subscriptions[updateId]
+                .Request()
+                .UpdateAsync(subscription);
+        }
+
+        private static string GetSubscriptionToUpdate(string notificationUrl, IGraphServiceSubscriptionsCollectionPage subscriptions) 
+            => subscriptions.Count > 0 && notificationUrl.Equals(subscriptions[0].NotificationUrl) ? subscriptions[0].Id : null;
     }
 }
